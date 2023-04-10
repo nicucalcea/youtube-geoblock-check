4bc7259c5da697b0a7cef1c77c2f5f0b74b55c80
nicucalcea <mail@nicu.md>
2023-04-11 00:00:44 +0100

    cleaned up code

--- contentScript.js
+++ contentScript.js
@@ -1,41 +1,10 @@
-let youtubeapi = "";
-
-function onError(error) {
-    console.log(`Error: ${error}`);
-}
-
-function onGot(item) {
-    if (item.youtubeapi) {
-        youtubeapi = item.youtubeapi;
-
-        // wait for the video to be fully loaded
-        const intervalId = setInterval(() => {
-            if (isVideoLoaded()) {
-                clearInterval(intervalId);
-                isGeoRestricted();
-            }
-        }, 100);
-
-
-    }
-}
-
-
-const getting = browser.storage.sync.get("youtubeapi");
-getting.then(onGot, onError);
-
-
 function getVideoId(url) {
     const urlObject = new URL(url);
     const pathname = urlObject.pathname;
     if (pathname.startsWith("/clip")) {
         return document.querySelector("meta[itemprop='videoId']").content;
-    } else {
-        if (pathname.startsWith("/shorts")) {
-            return pathname.slice(8);
-        }
-        return urlObject.searchParams.get("v");
     }
+    return pathname.startsWith("/shorts") ? pathname.slice(8) : urlObject.searchParams.get("v");
 }
 
 function isVideoLoaded() {
@@ -47,10 +16,7 @@ function isVideoLoaded() {
     );
 }
 
-
-
-// this function checks if the video is georestricted
-function isGeoRestricted() {
+function isGeoRestricted(youtubeapi) {
     const videoMessage = document.querySelector('#reason');
     if (videoMessage && videoMessage.textContent === 'Video unavailable') {
         const videoId = getVideoId(window.location.href);
@@ -59,19 +25,21 @@ function isGeoRestricted() {
             .then(response => response.json())
             .then(data => {
                 const blockedCountries = data.items[0].contentDetails.regionRestriction.blocked;
-                videoMessage.textContent = `This video is not available in your region. Blocked in ${blockedCountries.join(", ")}.`;
+                videoMessage.textContent = `This video is blocked in ${blockedCountries.join(", ")}.`;
 
             })
-            .catch(error => console.error('GEO: Error:', error));
+            .catch(error => console.warn('GEO: Error:', error));
     }
 }
 
-
-
-// wait for the video to be fully loaded
-const intervalId = setInterval(() => {
-    if (isVideoLoaded()) {
-        clearInterval(intervalId);
-        isGeoRestricted();
-    }
-}, 100);
+browser.storage.sync.get("youtubeapi")
+    .then(result => {
+        const youtubeapi = result.youtubeapi || "";
+        const intervalId = setInterval(() => {
+            if (isVideoLoaded()) {
+                clearInterval(intervalId);
+                isGeoRestricted(youtubeapi);
+            }
+        }, 100);
+    })
+    .catch(error => console.error('Storage Error:', error));
